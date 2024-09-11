b5ee3ce770d170672ab339279e8d11d290594162
Qwertyemma <emmanuelobiasogu32@gmail.com>
2024-09-11 16:14:27 +0100

    Update dashboardController.js

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,4 +1,14 @@
 // Render dashboard page
 exports.showDashboard = (req, res) => {
+    // Log to check if the function is being called
+    console.log('showDashboard function called');
+
+    // Log the session data to see if the username is present
+    console.log('Session data:', req.session);
+
+    // Log the username being passed to the template
+    console.log('Username:', req.session.username);
+
+    // Render the dashboard page
     res.render('dashboard', { title: 'Dashboard', username: req.session.username });
 };
